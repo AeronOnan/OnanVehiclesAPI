589ede7b6173396d9b54b734df8daaf03c65dd7b
Aeron Angel Onan <aerononan@gmail.com>
2022-04-08 01:33:03 +0800

    Created Model called Vehicls

--- myAPI/models.py
+++ myAPI/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class vehicles(models.Model):
+class vehicls(models.Model):
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=100)
 
